82c3511c4f6e8c4c9070fc7e592d51f6731bc6c9
Diniki <jayakodyjadn.19@uom.lk>
2022-11-27 23:34:42 +0530

    fts-back init

---
+++ src/main/java/com/example/ftsbackend/FtsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ftsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ftsbackend/FtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ftsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
